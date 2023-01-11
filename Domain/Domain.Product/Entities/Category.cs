682d994701cc536dc367fb70cf6ecebe7a779049
Juanjerrah <juan.oliveira@avonale.com.br>
2023-01-11 20:55:10 -0300

    feat: Criação das entidades

---
+++ Domain/Domain.Product/Entities/Category.cs
@@ -0,0 +1,33 @@
+﻿using Domain.Entities;
+using Domain.Product.Validation;
+using ProductDomain = Domain.Product.Entities.Product;
+
+namespace Domain.Product.Entities;
+public sealed class Category : Entity
+{
+    public string Name { get; private set; }
+    public IEnumerable<ProductDomain> Products { get; private set; }
+
+    public Category(string name)
+    {
+        ValidateCategoryDomain(name);
+    }
+
+    public Category(Guid id, string name)
+    {
+        DomainExceptionValidation.When(id.GetType() != typeof(Guid),"Invalid Id");
+        Id = id;
+        ValidateCategoryDomain(name);
+    }
+
+    public void UpdateCategory(string name) => ValidateCategoryDomain(name);
+    
+    private void ValidateCategoryDomain(string name)
+    {
+        DomainExceptionValidation.When(string.IsNullOrEmpty(name), 
+            "Invalid Name: Name is Required");
+        DomainExceptionValidation.When(name.Length <= 3,
+            "Invalid Name: Name must be at least 3 letters!");
+        Name = name;
+    }
+}
---
+++ Domain/Domain.Product/Entities/Entity.cs
@@ -0,0 +1,6 @@
+﻿namespace Domain.Entities;
+
+public class Entity
+{
+    public Guid Id { get; set; }
+}
