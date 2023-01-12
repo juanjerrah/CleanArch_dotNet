a153071bab4a52673045512f34ba32590ea6198b
Juanjerrah <juan.oliveira@avonale.com.br>
2023-01-11 21:05:26 -0300

    Mais

---
+++ Domain/Domain.Product/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Product.Entities;
+
+namespace Domain.Product.Interfaces;
+
+public interface ICategoryRepository
+{
+    Task<IEnumerable<Category>> GetCategories();
+    Task<Category?> GetCategoryById(Guid? id);
+    Task<Category> CreateCategory(Category Category);
+    Task<Category> UpdateCategory(Category Category);
+    Task<Category> DeleteCategory(Category Category);
+}
---
+++ Domain/Domain.Product/Interfaces/IProductRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Domain.Product.Interfaces;
+using ProductDomain = Entities.Product;
+public interface IProductDomainRepository
+{
+    Task<IEnumerable<ProductDomain?>> GetProductDomains();
+    Task<ProductDomain> GetProductDomainById(Guid? id);
+    Task<ProductDomain> GetProductDomainByCategory(Guid? id);
+    Task<ProductDomain?> CreateProductDomain(ProductDomain? productDomain);
+    Task<ProductDomain?> UpdateProductDomain(ProductDomain? productDomain);
+    Task<ProductDomain?> DeleteProductDomain(ProductDomain? productDomain);
+}
